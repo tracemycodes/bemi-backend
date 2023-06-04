73ae689d2975740c6d0f0a71e1c9995393945b08
Agba Emmanuel <agbaayoh@gmail.com>
2023-06-04 19:51:48 +0100

    app bug fixed

--- config/db.js
+++ config/db.js
@@ -1,10 +1,6 @@
 import mongoose from 'mongoose';
-import express from 'express';
-
-// const app = express();
 
 const connectDB = async () => {
-  const PORT = process.env.PORT || 5000;
   try {
     console.log('connect to db', process.env.MONGO_URI);
     const conn = await mongoose.connect(process.env.MONGO_URI, {
@@ -12,7 +8,6 @@ const connectDB = async () => {
       useNewUrlParser: true,
     });
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
-    // app.listen(PORT, console.log(`server running on port ${PORT}`.yellow.bold));
   } catch (err) {
     console.error(`Error: ${err.message}`.red.underline.bold);
     process.exit(1);
